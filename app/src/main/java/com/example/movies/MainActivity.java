97613f8a4f5454a9a03c96a1778f2ea11a56ca25
BIBLETUM <pasha.remizov@mail.ru>
2024-01-23 17:28:30 +0300

    Menu option for opening favourite movies added

--- app/src/main/java/com/example/movies/MainActivity.java
+++ app/src/main/java/com/example/movies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.movies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -9,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -74,4 +77,18 @@ public class MainActivity extends AppCompatActivity {
         moviesProgressBar = findViewById(R.id.moviesProgressBar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.itemFavouriteMovies){
+            Intent intent = ActivityFavMovies.newIntent(this);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
